046729918b99bc1e23ffb87cb7663f2006cba3e1
Archita <architapanda158@gmail.com>
2025-11-20 12:45:38 +0530

    Update footer.php

--- common/footer.php
+++ common/footer.php
@@ -1,30 +1,14 @@
-<!-- jQuery -->
 <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
-
-<!-- Bootstrap Bundle (includes Popper) -->
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js"></script>
-
-<!-- FullCalendar -->
 <script src="https://cdn.jsdelivr.net/npm/fullcalendar@6.1.15/index.global.min.js"></script>
-
-<!-- XLSX -->
-<script src="https://cdnjs.cloudflare.com/ajax/libs/xlsx/0.19.3/xlsx.full.min.js"></script>
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
 <script src="https://cdn.jsdelivr.net/npm/xlsx@0.18.5/dist/xlsx.full.min.js"></script>
-<script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
-<script src="https://cdn.jsdelivr.net/npm/chartjs-plugin-datalabels@2"></script>
 
 
-<!-- Vendor JS -->
 <script src="assets/js/vendor.min.js"></script>
-
-<!-- App JS -->
 <script src="assets/js/app.min.js"></script>
-<script src=assets/js/script.js></script>
-<script src=assets/js/project.js></script>
-<script src=assets/js/project_detail.js></script>
-<script src=assets/js/dashboard.js></script>
 
+<!-- ALWAYS LAST -->
+<script src="assets/js/script.js"></script>
 
 
 
