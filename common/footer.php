7b82d6f0a39123ad5ae2f799b880e4459415f364
Rojalin Sahoo <rojalin2022@gift.edu.in>
2025-12-02 14:28:12 +0530

    Update footer.php

--- common/footer.php
+++ common/footer.php
@@ -7,10 +7,7 @@
 
 <!-- BOOTSTRAP JS (bundle includes popper + dropdown support) -->
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
-<<<<<<< Updated upstream
 
-=======
->>>>>>> Stashed changes
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
 
 <script src="https://cdn.jsdelivr.net/npm/xlsx@0.18.5/dist/xlsx.full.min.js"></script>
@@ -18,11 +15,10 @@
 <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
 
 <script src="https://cdn.jsdelivr.net/npm/chartjs-plugin-datalabels@2"></script>
-<<<<<<< Updated upstream
 
 
-=======
->>>>>>> Stashed changes
+
+
 <script src="assets/js/vendor.min.js"></script>
 <script src="assets/js/app.min.js"></script>
 <!-- ALWAYS LAST -->
